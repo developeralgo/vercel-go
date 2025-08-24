f5797e025f1ab50a42d9e96d3ce15333c5538022
developer <mehrdad.anvar@gmail.com>
2025-08-24 14:46:26 -0700

    changed to new

--- api/index.go
+++ api/index.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -14,17 +13,12 @@ type Response struct {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-	// write the whole request including the headers to the response and return as json object
 	ip := r.RemoteAddr
-
-	// Create a map to hold the headers
 	headers := make(map[string][]string)
 	for name, values := range r.Header {
 		headers[name] = values
 	}
 
-	// Create the response object
 	response := Response{
 		IP:      ip,
 		Headers: headers,
@@ -32,11 +26,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		URL:     r.URL.String(),
 	}
 
-	// Set the content type to application/json
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	// Encode the response as JSON and send it
 	json.NewEncoder(w).Encode(response)
-
 }
